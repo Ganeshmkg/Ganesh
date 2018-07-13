18a8d017efe47b6df06327f7fb61914db31aeb98
Ganeshmkg <41116680+Ganeshmkg@users.noreply.github.com>
2018-07-13 12:30:59 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,14 @@
+package Ganesh;
+import java.util.Scanner;
+public class Count {
+	public static void main(String[] args) {
+		int a,count=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		while(a!=0){
+			a=a/10;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
