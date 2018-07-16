692357d09e1189d314d328575fc9b0a03739c67b
Ganeshmkg <41116680+Ganeshmkg@users.noreply.github.com>
2018-07-16 18:13:35 +0530

    Add files via upload

---
+++ StringSame.java
@@ -0,0 +1,11 @@
+package Ganesh;
+import java.util.Scanner;
+public class StringSame {
+	public static void main(String[] args) {
+		String a;
+		Scanner in=new Scanner(System.in);
+		a=in.nextLine();
+		System.out.println(a);
+	}
+
+}
