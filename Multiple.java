3697b609451ca6014246dc51f2ab25cd27452609
Ganeshmkg <41116680+Ganeshmkg@users.noreply.github.com>
2018-07-17 14:17:41 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,12 @@
+package Ganesh;
+import java.util.Scanner;
+public class Multiple {
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		c=a*b;
+		System.out.println(c);
+	}
+}
